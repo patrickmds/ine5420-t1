56829a5eb0fe4d116c8862d5233d0c57ac0754de
Gustavo Moser <gustavovbmoser@gmail.com>
2022-05-02 11:01:54 -0300

    printing button clicks

--- src/view/window.py
+++ src/view/window.py
@@ -7,21 +7,30 @@ def open_window():
     layout = [
         [
             sg.Column(menu_column(), vertical_alignment="t"),
-            sg.Column([[sg.Text("Column1", background_color="red", size=(100, 40))]]),
+            sg.Canvas(background_color="white", size=(600, 600), k="viewport"),
             sg.Column(item_list(), vertical_alignment="t"),
         ],
     ]
 
     # Create the window
     window = sg.Window("Column and Frame", layout)
+    viewport = window["viewport"]
 
     # Create an event loop
     while True:
         event, values = window.read()
         # End program if user closes window or
         # presses the OK button
-        if event == "OK" or event == sg.WIN_CLOSED:
+        if event == sg.WIN_CLOSED:
             break
+        elif event == "select":
+            print("select op is selected")
+        elif event == "point":
+            print("point op is selected")
+        elif event == "line":
+            print("line op is selected")
+        elif event == "wireframe":
+            print("wireframe op is selected")
 
     window.close()
 
@@ -30,10 +39,10 @@ def menu_column():
     # coluna com botões das funcionalidades
     sz = (10, 1)
     return [
-        [sg.Button("Select", size=sz)],
-        [sg.Button("Point", size=sz)],
-        [sg.Button("Line", size=sz)],
-        [sg.Button("Wireframe", size=sz)],
+        [sg.Button("Select", size=sz, k="select")],
+        [sg.Button("Point", size=sz, k="point")],
+        [sg.Button("Line", size=sz, k="line")],
+        [sg.Button("Wireframe", size=sz, k="wireframe")],
     ]
 
 
