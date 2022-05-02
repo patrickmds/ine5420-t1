4870c47520182e112ddc394bffe1717cf3945d44
Gustavo Moser <gustavovbmoser@gmail.com>
2022-05-02 14:48:22 -0300

    graph line

--- src/view/window.py
+++ src/view/window.py
@@ -4,17 +4,45 @@ import PySimpleGUI as sg
 def open_window():
     sg.theme("dark grey 9")
 
+    viewport_size = (600, 600)
+    top_right = (300, 300)
+    bottom_left = (-300, -300)
+
+    items = []
+
     layout = [
         [
             sg.Column(menu_column(), vertical_alignment="t"),
-            sg.Canvas(background_color="white", size=(600, 600), k="viewport"),
-            sg.Column(item_list(), vertical_alignment="t"),
+            sg.Graph(
+                canvas_size=viewport_size,
+                graph_bottom_left=bottom_left,
+                graph_top_right=top_right,
+                background_color="white",
+                border_width=1,
+                k="viewport",
+            ),
+            sg.Column(
+                [
+                    [
+                        sg.Listbox(
+                            values=items,
+                            select_mode=sg.SELECT_MODE_EXTENDED,
+                            size=(20, 40),
+                        )
+                    ]
+                ],
+                vertical_alignment="t",
+            ),
         ],
     ]
 
     # Create the window
-    window = sg.Window("Column and Frame", layout)
+    window = sg.Window("Graphics Computing", layout, finalize=True)
     viewport = window["viewport"]
+
+    # drawing vp line
+    viewport.draw_line((0, -300), (0, 300))  # origin
+    viewport.draw_line((-300, 0), (300, 0))
 
     # Create an event loop
     while True:
@@ -43,17 +71,4 @@ def menu_column():
         [sg.Button("Point", size=sz, k="point")],
         [sg.Button("Line", size=sz, k="line")],
         [sg.Button("Wireframe", size=sz, k="wireframe")],
-    ]
-
-
-def item_list():
-    # testando lista de itens
-    return [
-        [
-            sg.Listbox(
-                values=["1", "2", "3"],
-                select_mode=sg.SELECT_MODE_EXTENDED,
-                size=(20, 40),
-            )
-        ]
     ]
