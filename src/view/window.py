35a55321b33770771210eaee6f50a22794f20688
Gustavo Moser <gustavovbmoser@gmail.com>
2022-05-03 20:45:28 -0300

    reset, comments and fix line

--- src/view/window.py
+++ src/view/window.py
@@ -67,11 +67,11 @@ def open_window():
                     ],
                     [
                         sg.Button(
-                            "clear",
+                            "Reset",
                             size=(20, 10),
                             button_color="white",
                             enable_events=True,
-                            k="-clear-",
+                            k="-reset-",
                         ),
                     ],
                 ],
@@ -87,18 +87,38 @@ def open_window():
 
     viewport: sg.Graph = window["-viewport-"]
     viewport.bind("<Button-1>", "+LEFT")
+    viewport.bind("<Button-4>", "-WHEEL")
+    viewport.bind("<Button-5>", "+WHEEL")
+    # viewport.bind("<MouseWheel>", "-WHEEL")
 
     # drawing vp line
     draw_graph_axis_and_ticks(viewport, viewport_x, viewport_y, viewport_step)
 
-    dragging = False
+    drawing = False
     start_point = end_point = None
     # Create an event loop
     while True:
         event, values = window.read()
         # End program if user closes window
+
+        print(event)
         if event == sg.WIN_CLOSED:
             break
+
+        if event == "-zoom-in-":
+            """Event to zoom in the viewport"""
+            break
+
+        if event == "-zoom-out-":
+            """Event to zoom out the viewport"""
+            break
+
+        if event == "-reset-":
+            """Event to reset the viewport"""
+            items = []
+            window.find_element("-itemlist-").update(values=items)
+            viewport.erase()
+            draw_graph_axis_and_ticks(viewport, viewport_x, viewport_y, viewport_step)
 
         if event in [
             "-select-",
@@ -106,50 +126,70 @@ def open_window():
             "-line-",
             "-wireframe-",
         ]:
+            """Event to change graphic element"""
             window.find_element(active_button).update(button_color="white")
             active_button = event
+
+            if start_point is not None and drawing is True:
+                """Clean unfinished lines"""
+                it = viewport.get_figures_at_location(start_point)
+                print(it)
+                for id in it:
+                    viewport.delete_figure(id)
+                drawing = False
+                start_point = end_point = lastxy = None
+
             window.find_element(active_button).update(button_color="yellow")
 
         if event == "Delete":
+            """Event to delete elements from sidelist"""
             for item in values["-itemlist-"]:
                 viewport.delete_figure(item["id"])
                 items.remove(item)
             window.find_element("-itemlist-").update(values=items)
 
         if event.startswith("-viewport-"):
+            """Event ocurring inside viewport"""
             x, y = values["-viewport-"]
             # print(x, y, event)
 
             if active_button == "-point-" and event.endswith("+LEFT"):
-                point = viewport.draw_point((x, y), color="red")
+                """Event to plot a point from mouse click"""
+                point = viewport.draw_point((x, y), size=2, color="red")
                 items.append({"id": point, "type": "point", "x": x, "y": y})
                 window.find_element("-itemlist-").update(values=items)
 
             if active_button == "-line-":
-                if not event.endswith("+MOVE"):
-                    print(event)
-                if event.endswith("+LEFT") and not dragging:
+                """Event to plot a line"""
+                if event.endswith("+LEFT") and not drawing:
+                    """Start drawing a line"""
                     start_point = (x, y)
-                    dragging = True
+                    drawing = True
                     line = viewport.draw_line(
                         start_point, start_point, color="blue", width=2
                     )
                     lastxy = x, y
-                elif event.endswith("+UP") and (start_point != lastxy) and dragging:
+
+                elif event.endswith("+UP") and (start_point != lastxy) and drawing:
+                    """End drawing a line"""
                     end_point = (x, y)
                     viewport.delete_figure(line)
-                    viewport.draw_line(start_point, end_point, color="red", width=2)
-                    dragging = False
+                    line = viewport.draw_line(
+                        start_point, end_point, color="red", width=2
+                    )
+                    drawing = False
                     items.append(
                         {
-                            "id": line + 1,
+                            "id": line,
                             "type": "line",
                             "start": start_point,
                             "end": end_point,
                         }
                     )
                     window.find_element("-itemlist-").update(values=items)
-                elif dragging:
+
+                elif drawing:
+                    """Event while drawing a line"""
                     lastxy = x, y
                     viewport.delete_figure(line)
                     line = viewport.draw_line(
