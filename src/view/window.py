c3c4f018e6fc79e6ea7a3d4f08961f12e5e4b9d9
Gustavo Moser <gustavovbmoser@gmail.com>
2022-05-02 23:39:30 -0300

    add color when click button and graph axis

--- src/view/window.py
+++ src/view/window.py
@@ -4,22 +4,28 @@ import PySimpleGUI as sg
 def open_window():
     sg.theme("dark grey 9")
 
+    active_button = "-select-"
+
     viewport_size = (600, 600)
-    top_right = (300, 300)
-    bottom_left = (-300, -300)
+    viewport_x = viewport_size[0] / 2
+    viewport_y = viewport_size[1] / 2
+
+    top_right = (viewport_x, viewport_y)
+    bottom_left = (-viewport_x, -viewport_y)
 
     items = []
 
     layout = [
         [
-            sg.Column(menu_column(), vertical_alignment="t"),
+            sg.Column(menu_column(), vertical_alignment="t", k="-buttons-"),
             sg.Graph(
                 canvas_size=viewport_size,
                 graph_bottom_left=bottom_left,
                 graph_top_right=top_right,
                 background_color="white",
                 border_width=1,
-                k="viewport",
+                enable_events=True,
+                k="-viewport-",
             ),
             sg.Column(
                 [
@@ -37,38 +43,42 @@ def open_window():
     ]
 
     # Create the window
-    window = sg.Window("Graphics Computing", layout, finalize=True)
-    viewport = window["viewport"]
+    window = sg.Window(
+        "Graphics Computing", layout, use_default_focus=False, finalize=True
+    )
+    viewport = window["-viewport-"]
 
     # drawing vp line
-    viewport.draw_line((0, -300), (0, 300))  # origin
-    viewport.draw_line((-300, 0), (300, 0))
+    viewport.draw_line((0, -viewport_y), (0, viewport_y), color="red")  # origin
+    viewport.draw_line((-viewport_x, 0), (viewport_x, 0), color="red")
 
     # Create an event loop
     while True:
         event, values = window.read()
-        # End program if user closes window or
-        # presses the OK button
+        # End program if user closes window
         if event == sg.WIN_CLOSED:
             break
-        elif event == "select":
-            print("select op is selected")
-        elif event == "point":
-            print("point op is selected")
-        elif event == "line":
-            print("line op is selected")
-        elif event == "wireframe":
-            print("wireframe op is selected")
+        elif event in [
+            "-select-",
+            "-point-",
+            "-line-",
+            "-wireframe-",
+        ]:
+            window.find_element(active_button).update(button_color="white")
+            active_button = event
+            window.find_element(active_button).update(button_color="yellow")
+
+        if event == "-viewport-":
+            print("click on graph")
 
     window.close()
 
 
 def menu_column():
-    # coluna com botões das funcionalidades
     sz = (10, 1)
     return [
-        [sg.Button("Select", size=sz, k="select")],
-        [sg.Button("Point", size=sz, k="point")],
-        [sg.Button("Line", size=sz, k="line")],
-        [sg.Button("Wireframe", size=sz, k="wireframe")],
+        [sg.Button("Select", size=sz, enable_events=True, k="-select-")],
+        [sg.Button("Point", size=sz, enable_events=True, k="-point-")],
+        [sg.Button("Line", size=sz, enable_events=True, k="-line-")],
+        [sg.Button("Wireframe", size=sz, enable_events=True, k="-wireframe-")],
     ]
